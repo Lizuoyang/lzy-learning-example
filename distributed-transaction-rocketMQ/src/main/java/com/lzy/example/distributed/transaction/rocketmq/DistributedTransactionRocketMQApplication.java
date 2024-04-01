28176002753db763456b6791e9381395e275d4d5
lizuoyang <939822143@qq.com>
2024-04-01 13:48:48 +0800

    添加模块

---
+++ distributed-transaction-rocketMQ/src/main/java/com/lzy/example/distributed/transaction/rocketmq/DistributedTransactionRocketMQApplication.java
@@ -0,0 +1,16 @@
+package com.lzy.example.distributed.transaction.rocketmq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * springboot 启动类
+ * @author Lizuoyang
+ * @date 2024/04/01
+ */
+@SpringBootApplication
+public class DistributedTransactionRocketMQApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(DistributedTransactionRocketMQApplication.class, args);
+    }
+}

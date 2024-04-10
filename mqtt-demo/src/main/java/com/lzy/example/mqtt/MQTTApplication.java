83a7feddcc9910504ed632b0f8c0a4efed4501b6
lizuoyang <939822143@qq.com>
2024-04-10 17:48:11 +0800

    添加模块

---
+++ mqtt-demo/src/main/java/com/lzy/example/mqtt/MQTTApplication.java
@@ -0,0 +1,16 @@
+package com.lzy.example.mqtt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * springboot 启动类
+ * @author Lizuoyang
+ * @date 2024/04/09
+ */
+@SpringBootApplication
+public class MQTTApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MQTTApplication.class, args);
+    }
+}
